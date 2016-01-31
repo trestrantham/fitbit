e895a5523e7d43edf53583510c5accb37e7a4278
Tres Trantham <tres@trestrantham.com>
2016-01-31 00:07:59 -0800

    Add struct parsing for user

--- lib/fitbit/user.ex
+++ lib/fitbit/user.ex
@@ -1,9 +1,58 @@
 defmodule Fitbit.User do
+  alias Fitbit.Utils
+
+  defstruct [
+    :about_me, :avatar, :avatar150, :city, :country, :date_of_birth,
+    :display_name, :distance_unit, :encoded_id, :foods_locale, :full_name,
+    :gender, :glucose_unit, :height, :height_unit, :locale, :member_since,
+    :nickname, :offset_from_utc_millis, :start_day_of_week, :state,
+    :stride_length_running, :stride_length_walking, :timezone, :water_unit,
+    :weight, :weight_unit
+  ]
+
   def profile(user_token) do
-    Fitbit.user_request(:get, "profile", user_token)
+    case Fitbit.user_request(:get, "profile", user_token) do
+      {:ok, body} ->
+        body["user"]
+        |> parse_user
+      error ->
+        error
+    end
   end
 
   def badges(user_token) do
     Fitbit.user_request(:get, "badges", user_token)
+  end
+
+  defp parse_user(user) do
+    %Fitbit.User{
+      about_me: user["aboutMe"],
+      avatar: user["avatar"],
+      avatar150: user["avatar150"],
+      city: user["city"],
+      country: user["country"],
+      date_of_birth: Utils.parse_date(user["dateOfBirth"]),
+      display_name: user["displayName"],
+      distance_unit: user["distanceUnit"],
+      encoded_id: user["encodedId"],
+      foods_locale: user["foodsLocale"],
+      full_name: user["fullName"],
+      gender: Utils.parse_gender(user["gender"]),
+      glucose_unit: user["glucoseUnit"],
+      height: user["height"],
+      height_unit: user["heightUnit"],
+      locale: user["locale"],
+      member_since: Utils.parse_date(user["memberSince"]),
+      nickname: user["nickname"],
+      offset_from_utc_millis: user["offsetFromUTCMillis"],
+      start_day_of_week: Utils.parse_day(user["startDayOfWeek"]),
+      state: user["state"],
+      stride_length_running: user["strideLengthRunning"],
+      stride_length_walking: user["strideLengthWalking"],
+      timezone: user["timezone"],
+      water_unit: user["waterUnit"],
+      weight: user["weight"],
+      weight_unit: user["weightUnit"]
+    }
   end
 end
